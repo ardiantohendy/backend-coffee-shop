eb84d5465960d40976c6e8e5eacc684ee87913e8
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-16 01:05:17 +0700

    create order's models

---
+++ order/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from menu.models import MenuItem
+
+# Create your models here.
+
+User = get_user_model()
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(MenuItem, through='OrderItem')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order {self.id} by {self.user.username}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.quantity}x {self.menu_item.name} in Order {self.order.id}'
