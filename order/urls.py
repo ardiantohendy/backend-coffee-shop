935116fa9e528b1bd433ee093022fee7ab5e52f5
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-16 16:54:05 +0700

    fixing bug in order feature

--- order/urls.py
+++ order/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'order', views.OrderViewSet)
+router.register(r'orders', views.OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
