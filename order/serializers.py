b6608aec348449a671aae1f21bdb30627e767496
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-19 00:19:42 +0700

    new commit fixing bug

--- order/serializers.py
+++ order/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
+from menu.models import MenuItem
 
 class OrderItemSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username', read_only=True)
@@ -19,6 +20,21 @@ class OrderSerializer(serializers.ModelSerializer):
         items_data = validated_data.pop('items', [])
         user = self.context['request'].user 
 
-        order = Order.objects.get(id=item_data['menu_item'])
+        order = Order.objects.create(user=user, **validated_data)
+        total_price = 0
+
+
+        for item_data in items_data:
+            menu_item = MenuItem.objects.get(id=item_data['menu_item'])
+            quantity = item_data['quantity']
+            price = menu_item.price * quantity
+            total_price += price
+
+            OrderItem.objects.create(order=order, menu_item=menu_item, quantity=quantity)
+
+        order.total_price = total_price
+        order.save()
+
+        return order
         
         
