c83c3aba74d89cfe10f9350f69fd5d246e7eb07e
Ardiantohendy <bayuardianto421@gmail.com>
2025-04-26 11:29:58 +0700

    new commit add validate_email for check if email already exists

--- users/serializers.py
+++ users/serializers.py
@@ -10,11 +10,16 @@ class UserSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
-
+    
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'phone_number', 'password', 'password2']
 
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with that Email already exists.")
+        return value
+    
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({'password': 'password didn\'t match.'})
